db35790ea11411586ade2ead365568228494c92a
OpKotor <informatika@kotor.me>
2025-05-05 09:05:57 +0200

    Update validacija_vaucera.php

--- validacija_vaucera.php
+++ validacija_vaucera.php
@@ -1,5 +1,5 @@
 <?php
-// Provera vaučera
+// Provjera vaučera
 $vaucer = $_POST['vaucer_kod'];
 
 $sql = "SELECT * FROM vaucers WHERE kod = '$vaucer' AND iskoriscen = 0";
@@ -7,7 +7,7 @@ $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     echo "Vaučer je validan!";
-    // Obeleži vaučer kao iskorišćen
+    // Obilježi vaučer kao iskorišćen
     $sql = "UPDATE vaucers SET iskoriscen = 1 WHERE kod = '$vaucer'";
     $conn->query($sql);
 } else {
